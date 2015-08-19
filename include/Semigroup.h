eb67edc959c0a05c919f6c29e2e6708514a4b3da
Lukas Huwald <lukashuwald@online.de>
2015-08-19 13:05:06 +0200

    added semigroup class files

---
+++ include/Semigroup.h
@@ -0,0 +1,26 @@
+#ifndef OMALG_SEMIGROUP
+#define OMALG_SEMIGROUP
+
+#include <vector>
+#include <string>
+
+namespace omalg {
+
+  class Semigroup {
+  private:
+    int numberOfElements;
+    std::vector<std::string> elementNames;
+    std::vector<std::vector<int> > multiplicationTable;
+    UnionFind* rClasses;
+    UnionFind* lClasses;
+    UnionFind* jClasses;
+    UnionFind* hClasses;
+    std::vector<std::vector<bool> >* jOrder;
+  public:
+    void calculateGreenRelations();
+    void calculateJOrder();
+  };
+
+}
+
+#endif
