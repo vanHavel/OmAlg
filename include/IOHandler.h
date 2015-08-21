b7fb9da263411497c813333725cf92e95c70b398
Lukas Huwald <lukashuwald@online.de>
2015-08-21 12:41:08 +0200

    added more diverse IO method declarations

--- include/IOHandler.h
+++ include/IOHandler.h
@@ -2,6 +2,7 @@
 #define OMALG_IO_HANDLER
 
 #include <string>
+#include <iostream>
 
 #include "OmegaAutomaton.h"
 #include "BuechiAutomaton.h"
@@ -11,15 +12,31 @@
 namespace omalg {
 
   class IOHandler {
+  private:
+	  OmegaAutomaton readAutomatonFromStream(std::istream &in);
+	  void writeAutomatonToStream(OmegaAutomaton A, std::ostream &out);
+	  
+	  Semigroup readSemigroupFromStream(std:istream &in);
+	  void writeSemigroupToStream(Semigroup S, std::ostream &out);
+	  
+	  OmegaSemigroup readOmegaSemigroupFromStream(std::istream &in);
+	  void writeOmegaSemigroupToStream(OmegaSemigroup S, std::ostream &out);
+		  
   public: 
-	  OmegaAutomaton readAutomaton(std::string inputFileName);
-	  void writeAutomaton(OmegaAutomaton A, std::string outputFileName);
+	  OmegaAutomaton readAutomatonFromFile(std::string inputFileName);
+	  OmegaAutomaton readAutomatonFromStdin();
+	  void writeAutomatonToFile(OmegaAutomaton A, std::string outputFileName);
+	  void writeAutomatonToStdout(OmegaAutomaton A);
 	  
-	  Semigroup readSemigroup(std::string inputFileName);
-	  void writeSemigroup(Semigroup S, std::string outputFileName);
+	  Semigroup readSemigroupFromFile(std::string inputFileName);
+	  Semigroup readSemigroupFromStdin();
+	  void writeSemigroupToFile(Semigroup S, std::string filename);
+	  void writeSemigroupToStdout(Semigroup S);
 	  
-	  OmegaSemigroup readOmegaSemigroup(std::string inputFileName);
-	  void writeOmegaSemigroup(OmegaSemigroup S, std::string outputFileName);
+	  OmegaSemigroup readOmegaSemigroupFromFile(std::string inputFileName);
+	  OmegaSemigroup readOmegaSemigroupFromStdin();
+	  void writeOmegaSemigroupToFile(OmegaSemigroup S, std::string outputFileName);
+	  void writeOmegaSemigroupToStdout(OmegaSemigroup S);
   };
 
 }
